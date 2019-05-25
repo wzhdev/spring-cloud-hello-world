5e36c3430f7f9df111df79ebb6705c637465c07c
wzhdev <wzh_dev@163.com>
2019-05-25 11:38:14 +0800

    init

---
+++ src/main/java/com/wzh/dev/spring/cloud/springcloudhelloworld/SpringCloudHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.wzh.dev.spring.cloud.springcloudhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wzh/dev/spring/cloud/springcloudhelloworld/SpringCloudHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wzh.dev.spring.cloud.springcloudhelloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
